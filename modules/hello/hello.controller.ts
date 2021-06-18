4f60efd79bc91ab72c0679b3eebbdfd375f58ee7
edwinzeng <edwinzeng@wesure.cn>
2021-06-19 00:32:09 +0800

    delete Cache

--- modules/hello/hello.controller.ts
+++ modules/hello/hello.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get, Post } from '@nestjs/common'
+import { Controller, Get } from '@nestjs/common'
 import { HelloService } from './hello.service'
-import { Cache } from '../_common/Cache.service'
 
 
 @Controller('/hello')
@@ -8,20 +7,11 @@ export class HelloController {
 
     constructor(
         private readonly helloWorldService: HelloService,
-        private readonly cache: Cache,
     ) {}
 
     @Get('/sayHello')
     async sayHello() {
         const res = await this.helloWorldService.sayHello()
-        return res
-    }
-
-    @Post('/sayHi')
-    async sayHi() {
-        const res = await this.cache.cacheFactory({ cacheKey: 'cacheSayHi' }, () => {
-            return this.helloWorldService.sayHello()
-        })
         return res
     }
     
